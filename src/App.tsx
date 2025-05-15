064a47fe6e194c5f4adcf5ca7b5089048013cee2
bcardea <122843429+bcardea@users.noreply.github.com>
2025-05-15 13:23:17 -0400

    Restore Supabase connection and remove API key input

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,23 @@ import GeneratorPage from './pages/GeneratorPage';
 import LibraryPage from './pages/LibraryPage';
 import TermsPage from './pages/TermsPage';
 import PrivacyPage from './pages/PrivacyPage';
-import { ApiKeyProvider } from './context/ApiKeyContext';
 
 function App() {
   return (
-    <ApiKeyProvider>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/generator" element={<GeneratorPage />} />
-            <Route path="/library" element={<LibraryPage />} />
-            <Route path="/terms" element={<TermsPage />} />
-            <Route path="/privacy" element={<PrivacyPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </ApiKeyProvider>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/generator" element={<GeneratorPage />} />
+          <Route path="/library" element={<LibraryPage />} />
+          <Route path="/terms" element={<TermsPage />} />
+          <Route path="/privacy" element={<PrivacyPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
-export default App
+export default App;

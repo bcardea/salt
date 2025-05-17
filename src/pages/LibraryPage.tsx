62fdaa226ab27a9789343aedbaf37bacdc908bd1
bcardea <122843429+bcardea@users.noreply.github.com>
2025-05-17 07:28:27 -0400

    Add lazy loading to library images

--- src/pages/LibraryPage.tsx
+++ src/pages/LibraryPage.tsx
@@ -105,6 +105,7 @@ const LibraryPage: React.FC = () => {
                     src={image.url}
                     alt={image.topic}
                     className="w-full h-full object-cover"
+                    loading="lazy"
                   />
                   <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-200">
                     <Download className="w-8 h-8 text-white" />
